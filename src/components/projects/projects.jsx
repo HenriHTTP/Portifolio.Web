b3f5cfbaf38668ea6180557211719726b745cde6
Henrique da Silva <86864451+HenriHTTP@users.noreply.github.com>
2023-03-05 12:00:04 -0300

    card  tiltt and  link

--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -1,6 +1,8 @@
 import './style/projects.css'
 import  CEP from './img/CEP.png'
 import Anime from './img/anime.png'
+import Tilt from 'react-parallax-tilt'
+
 
 export default function Project(){ 
     return(
@@ -9,25 +11,30 @@ export default function Project(){
                 <div className="Titles">
                     <h1 className='Projecth1'> Projetos</h1>
                     <div className="containerCards">
-                        
-                    <div className="CardProjecs">
-                        <h1>Ladding page</h1>
+                    <Tilt>
+                    <div className="CardProjecs" onClick={() => window.open('https://anime-henrihttp.vercel.app/')}>
+                        <h1>Buscador de animes</h1>
                         <img src={Anime} alt="img" srcset="" />
                         <p>Ao acessar, o usuário poderá buscar pelo título do anime e ter acesso a uma série de dados como notas e avaliações de outros usuários, entre outros.</p>
                         <button  onClick={() => window.open('https://anime-henrihttp.vercel.app/')}>Ver mais</button>
                     </div>
-                    <div className="CardProjecs">
+                    </Tilt>
+                    <Tilt>
+                    <div className="CardProjecs" onClick={() => window.open('https://search-cep-alpha.vercel.app/')}>
                         <h1>Buscador de CEP</h1>
                         <img src={CEP} alt="img" srcset="" />
                         <p>Ao inserir o CEP na interface, a aplicação faz uma requisição à API e retorna informações de logradouro, bairro, cidade e estado</p>
                         <button  onClick={() => window.open('https://search-cep-alpha.vercel.app/')}>Ver mais</button>
                     </div>
+                    </Tilt>
+                    <Tilt>
                     <div className="CardProjecs">
                         <h1>Academia</h1>
                         <img src="" alt="img" srcset="" />
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
                         <button>Ver mais</button>
                     </div>
+                    </Tilt>
                     </div>
                     </div>
             </div>
