2a3d6d3ebf44ba079544b173a92c6da931814963
Henrique da Silva <86864451+HenriHTTP@users.noreply.github.com>
2023-03-12 01:11:04 -0300

    cards project

--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -2,6 +2,8 @@ import './style/projects.css'
 import  CEP from './img/CEP.png'
 import Anime from './img/anime.png'
 import Tilt from 'react-parallax-tilt'
+import {SiMyanimelist} from 'react-icons/si'
+import{BsMap} from 'react-icons/bs'
 
 
 export default function Project(){ 
@@ -14,23 +16,26 @@ export default function Project(){
                     <Tilt>
                     <div className="CardProjecs" onClick={() => window.open('https://anime-henrihttp.vercel.app/')}>
                         <h1>Buscador de animes</h1>
-                        <img src={Anime} alt="img" srcset="" />
-                        <p>Ao acessar, o usuário poderá buscar pelo título do anime e ter acesso a uma série de dados como notas e avaliações de outros usuários, entre outros.</p>
+                        <h2><SiMyanimelist/></h2>
+                     
+                        <p>Ao acessar, o usuário poderá buscar pelo título do anime e ter acesso a uma série de dados como notas e avaliações de outros usuários, entre outros atributos do anime pesquisado.</p>
                         <button  onClick={() => window.open('https://anime-henrihttp.vercel.app/')}>Ver mais</button>
                     </div>
                     </Tilt>
                     <Tilt>
                     <div className="CardProjecs" onClick={() => window.open('https://search-cep-alpha.vercel.app/')}>
                         <h1>Buscador de CEP</h1>
-                        <img src={CEP} alt="img" srcset="" />
-                        <p>Ao inserir o CEP na interface, a aplicação faz uma requisição à API e retorna informações de logradouro, bairro, cidade e estado</p>
+                        <h2><BsMap/></h2>
+                        
+                        <p>Ao inserir o CEP na interface, a aplicação faz uma requisição à API e retorna informações de logradouro, bairro, cidade e estado ao usuario</p>
                         <button  onClick={() => window.open('https://search-cep-alpha.vercel.app/')}>Ver mais</button>
                     </div>
                     </Tilt>
                     <Tilt>
                     <div className="CardProjecs">
                         <h1>Academia</h1>
-                        <img src="" alt="img" srcset="" />
+                        
+                       
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
                         <button>Ver mais</button>
                     </div>
