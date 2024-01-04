8a3317057ed211f0f3a6c6b27bd36473c2c10c22
Henrique da Silva <86864451+HenriHTTP@users.noreply.github.com>
2024-01-04 15:25:56 -0300

    refactor: remove duplicate func and use const

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -1,90 +1,88 @@
-import React, { useState,useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 import './styles/navbar.css'
 import {BsLinkedin} from 'react-icons/bs'
-import { Link } from 'react-scroll';
+import {Link} from 'react-scroll';
 import {FaUserAlt} from 'react-icons/fa'
-import {BsHeadphones,BsLaptopFill} from 'react-icons/bs'
-import {AiFillHome,AiOutlineClose,AiFillProject} from 'react-icons/ai'
+import {BsHeadphones, BsLaptopFill} from 'react-icons/bs'
+import {AiFillHome, AiFillProject} from 'react-icons/ai'
 import {GiHamburgerMenu} from 'react-icons/gi'
-export default function Navbar(){
 
-    const [menuclass,setmenuclass]= useState('navbar')
+export default function Navbar() {
+
+    const [MenuClass, SetMenuClass] = useState('navbar')
+    const [MenuState, SetManuState ] = useState(true)
+
     useEffect(() => {
-    // Utilizando useEffect para adicionar o listener de scroll ao carregar o componente
-    window.addEventListener('scroll', handleScroll);
-
-    // Utilizando useEffect para remover o listener de scroll ao desmontar o componente
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
+        window.addEventListener('scroll', handleScroll);
+       return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
     }, [])
-
-    // função  para mudar classe de compenente nav
-    
-    let handleScroll = () => { 
-    window.scrollY >= 1 ? setmenuclass('fixed') :setmenuclass('navbar')
+    const handleScroll = () => {
+        window.scrollY >= 1 ? SetMenuClass('fixed') : SetMenuClass('navbar')
     }
+     const HandleMenu = () => {
+        SetManuState(!MenuState);
+        const icons = document.getElementById('icons')
 
-    // Função para exibir o menu
+        const IsOpen = MenuState === true;
+        if (IsOpen) {
+            icons.style.display = 'flex'
+        }
 
-    let HandleMenuOn = ( ) => { 
-        var icons = document.getElementById('icons')
-        var open = document.getElementById('open')
-        var close = document.getElementById('close')
-
-        icons.style.display = 'flex'
-        close.style.display = 'block'
-        open.style.display = 'none'
+        const IsClose = MenuState === false;
+        if (IsClose) {
+            icons.style.display = 'none'
+        }
     }
-
-    //funcão   para  ocultar menu 
-
-    let HandleMenuOff = ( ) => { 
-        var icons = document.getElementById('icons')
-        var open = document.getElementById('open')
-        var close = document.getElementById('close')
-
-        icons.style.display = 'none'
-        open.style.display = 'block'
-        close.style.display = 'none'
-        
-    }
-    return(
+    return (
         <>
-        <div className='Content' id='home'> 
-        <div id="nav" className={menuclass}>  
-        <div className='logo'>
-                <h1>&lt;he/&gt;  </h1>
+            <div className='Content' id='home'>
+                <div id="nav" className={MenuClass}>
+                    <div className='logo'>
+                        <h1>&lt;he/&gt;  </h1>
+                    </div>
+                    <div className='Links'>
+                        <ul>
+                            <li><Link activeClass="active" to="home" spy={true} smooth={true} offset={50}
+                                      duration={500}>Home</Link></li>
+                            <li><Link activeClass="active" to="about" spy={true} smooth={true} offset={50}
+                                      duration={500}>About</Link></li>
+                            <li><Link activeClass="active" to="tec" spy={true} smooth={true} offset={50}
+                                      duration={500}>Skills</Link></li>
+                            <li><Link activeClass="active" to="project" spy={true} smooth={true} offset={50}
+                                      duration={500}>Projects</Link></li>
+                            <li><Link activeClass="active" to="contacts" spy={true} smooth={true} offset={50}
+                                      duration={500}>Contact</Link></li>
+                        </ul>
+                        <div id="icons" className="icons-nav">
+                            <p><Link activeClass="active" to="home" spy={true} smooth={true} offset={50} duration={500}><AiFillHome/>
+                            </Link></p>
+                            <p><Link activeClass="active" to="about" spy={true} smooth={true} offset={50}
+                                     duration={500}><FaUserAlt/></Link></p>
+                            <p><Link activeClass="active" to="tec" spy={true} smooth={true} offset={50}
+                                     duration={500}><BsLaptopFill/></Link></p>
+                            <p><Link activeClass="active" to="project" spy={true} smooth={true} offset={50}
+                                     duration={500}><AiFillProject/></Link></p>
+                            <p><Link activeClass="active" to="contacts" spy={true} smooth={true} offset={50}
+                                     duration={500}><BsHeadphones/></Link></p>
+                        </div>
+                        <button id="open" className="hamburger" onClick={HandleMenu}><GiHamburgerMenu/></button>
+                    </div>
+                </div>
+                <div className='register'>
+                    <main>
+                        <p>Hi,i'm Henrique Silva</p>
+                        <h2>Software developer</h2>
+                        <button className='linkedin'
+                                onClick={() => window.open('https://www.linkedin.com/in/henrique-s-da-silva-3115b1228/')}>
+                            <BsLinkedin/> Linkedin
+                        </button>
+                    </main>
+
+                </div>
+
             </div>
-            <div className='Links'>
-                <ul>
-                <li ><Link activeClass="active" to="home" spy={true} smooth={true} offset={50} duration={500}>Home</Link></li>
-                <li><Link activeClass="active" to="about" spy={true} smooth={true} offset={50} duration={500}>About</Link></li>
-                <li><Link activeClass="active" to="tec" spy={true} smooth={true} offset={50} duration={500}>Skills</Link></li>
-                <li><Link activeClass="active" to="project" spy={true} smooth={true} offset={50} duration={500}>Projects</Link></li>
-                <li><Link activeClass="active" to="contacts" spy={true} smooth={true} offset={50} duration={500}>Contact</Link></li>
-                </ul>
-                 <div id="icons"className="icons-nav">
-                    <p><Link activeClass="active" to="home" spy={true} smooth={true} offset={50} duration={500}><AiFillHome/> </Link></p>
-                    <p><Link activeClass="active" to="about" spy={true} smooth={true} offset={50} duration={500}><FaUserAlt/></Link> </p>
-                    <p><Link activeClass="active" to="tec" spy={true} smooth={true} offset={50} duration={500}><BsLaptopFill/></Link> </p>
-                    <p><Link activeClass="active" to="project" spy={true} smooth={true} offset={50} duration={500}><AiFillProject/></Link> </p>
-                    <p><Link activeClass="active" to="contacts" spy={true} smooth={true} offset={50} duration={500}><BsHeadphones/></Link> </p>
-                 </div>
-                 <button id="open" className="hamburger" onClick={HandleMenuOn}><GiHamburgerMenu/></button>
-                 <button id="close" className="Esc" onClick={HandleMenuOff}><AiOutlineClose/></button>
-            </div>
-        </div>
-        <div className='register'>
-            <main>
-            <p>Hi,i'm Henrique Silva</p>
-            <h2>Software developer</h2>
-            <button className='linkedin' onClick={() => window.open('https://www.linkedin.com/in/henrique-s-da-silva-3115b1228/')} > <BsLinkedin/> Linkedin</button>
-            </main>
-            
-        </div>
-           
-        </div>
         </>
     )
 }
