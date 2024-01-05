28b0ea8a88b8a8edaccb6571032eaecc8e0af2c5
Henrique da Silva <86864451+HenriHTTP@users.noreply.github.com>
2024-01-04 23:29:59 -0300

    feat: add new cards

--- src/components/baseboard/contact.jsx
+++ src/components/baseboard/contact.jsx
@@ -1,32 +1,25 @@
 import React from 'react'
 import './style/contact.css'
-import {AiOutlineWhatsApp,AiFillGithub,AiFillLinkedin} from 'react-icons/ai'
+import {AiOutlineWhatsApp, AiFillGithub, AiFillLinkedin} from 'react-icons/ai'
 
 export default function Contact() {
-    const whatsapp = () => {
-        window.open('https://api.whatsapp.com/send?phone=551195804462')
-    }
-    const linkedin = () => {
-        window.open('https://www.linkedin.com/in/henrique-s-da-silva-3115b1228/')
-    }
-    const git = () => {
-        window.open('https://github.com/HenriHTTP')
-    }
-return (
-    <>
-    <div className='ContactContainer'>
-        
-        <div id='contacts'className='Contact'>
-        <h1>contact</h1>
-      
-        
-        <div className='ContactContent'>
-            <button  onClick={git}><AiFillGithub/> Git</button>
-            <button onClick={linkedin}><AiFillLinkedin/> Linkendin</button>
-            <button onClick={whatsapp}><AiOutlineWhatsApp/> WhatsApp</button>
-        </div>
-    </div>
-    </div>
-    </>
-)
+    return (
+        <>
+            <div className='ContactContainer'>
+                <div id='contacts' className='Contact'>
+                    <h1>contact</h1>
+                    <div className='ContactContent'>
+                        <button onClick={() => window.open('https://github.com/HenriHTTP')}><AiFillGithub/> Git</button>
+                        <button
+                            onClick={() => window.open('https://www.linkedin.com/in/henrique-s-da-silva-3115b1228/')}>
+                            <AiFillLinkedin/> Linkedin
+                        </button>
+                        <button onClick={() => window.open('https://api.whatsapp.com/send?phone=551195804462')}>
+                            <AiOutlineWhatsApp/> WhatsApp
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
