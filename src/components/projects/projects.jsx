2f05fd1215870251dcf42f29d7433b9ddd70371f
Henrique da Silva <86864451+HenriHTTP@users.noreply.github.com>
2024-03-14 20:20:22 -0300

    refactor: add new projects

--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -1,7 +1,8 @@
 import './style/projects.css'
-import {BiNotepad} from 'react-icons/bi'
+import {FaLongArrowAltRight} from "react-icons/fa";
 import '../global/styles.css'
-
+import {IoLogoPython} from "react-icons/io";
+import {IoLogoJavascript} from "react-icons/io5";
 
 export default function Project() {
     return (<>
@@ -14,48 +15,54 @@ export default function Project() {
                         <div data-aos="fade-right" className="Card"
                              onClick={() => window.open('https://github.com/HenriHTTP/express-mongo-docker-api')}>
                             <h1 className="subtitle"> express-mongo-docker-api</h1>
-                            <h2 className="title"><BiNotepad/></h2>
+                            <h2 className="title"><IoLogoJavascript/></h2>
                             <p className="text">Project made for learning rest api architecture, this api have services
                                 for user
                                 registration and login per token </p>
                             <button className="normal-button"
-                                    onClick={() => window.open('https://github.com/HenriHTTP/express-mongo-docker-api')}>see more
+                                    onClick={() => window.open('https://github.com/HenriHTTP/express-mongo-docker-api')}>
+                                <FaLongArrowAltRight/>
                             </button>
                         </div>
 
                         <div data-aos="fade-right" className="Card"
                              onClick={() => window.open('https://github.com/HenriHTTP/Portifolio.Web')}>
                             <h1 className="subtitle">portfolio.web</h1>
-                            <h2 className="title"><BiNotepad/></h2>
+                            <h2 className="title"><IoLogoJavascript/></h2>
                             <p className="text">This portfolio is crafted with React,using a single-page application
                                 (SPA) architecture to deliver a seamless and engaging user experience. </p>
 
                             <button className="normal-button"
                                     onClick={() => window.open('https://github.com/HenriHTTP/Portifolio.Web')}>
-                                see more
+                                <FaLongArrowAltRight/>
                             </button>
                         </div>
 
                         <div data-aos="fade-right" className="Card"
-                             onClick={() => window.open('https://github.com/HenriHTTP/ping-pong')}>
-                            <h1 className="subtitle">Ping-pong</h1>
-                            <h2 className="title"><BiNotepad/></h2>
-                            <p className="text">Ping pong game written in C++ , SDL2 and Glew </p>
+                             onClick={() => window.open('https://github.com/HenriHTTP/scrapinpy.py')}>
+                            <h1 className="subtitle">scrapinpy.py</h1>
+                            <h2 className="title"><IoLogoPython/></h2>
+                            <p className="text">The Scrapinpy.py is a tool designed to extract specific information,
+                                known as regulations or normative, available on the website gov.br </p>
 
                             <button className="normal-button"
-                                    onClick={() => window.open('https://github.com/HenriHTTP/ping-pong')}>see more
+                                    onClick={() => window.open('https://github.com/HenriHTTP/scrapinpy.py')}>
+                                <FaLongArrowAltRight/>
                             </button>
                         </div>
                         <div data-aos="fade-right" className="Card"
-                             onClick={() => window.open('https://github.com/HenriHTTP/sqlite-python')}>
+                             onClick={() => window.open('https://github.com/HenriHTTP/for-tomorrow-api')}>
                             <h1 className="subtitle">
-                                sqlite-python
+                                for-tomorrow-api
                             </h1>
-                            <h2 className="title"><BiNotepad/></h2>
-                            <p className="text">This application is crafted with SQLite and python </p>
+                            <h2 className="title"><IoLogoPython/></h2>
+                            <p className="text">Rest api written in Python, designed to provide various useful features
+                                for your manager RH. The API utilizes endpoints for create user, To receive background ,
+                                and storage archives. </p>
 
                             <button className="normal-button"
-                                    onClick={() => window.open('https://github.com/HenriHTTP/sqlite-python')}>see more
+                                    onClick={() => window.open('https://github.com/HenriHTTP/for-tomorrow-api')}>
+                                <FaLongArrowAltRight/>
                             </button>
                         </div>
 
